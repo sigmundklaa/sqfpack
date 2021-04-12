bc9baeb052d8b39808ae72437be2a46366fe201d
Sigmund Klåpbakken <sigmundklaa@outlook.com>
2021-04-12 21:56:05 +0200

    Context, more on module

---
+++ sqfpack/context.py
@@ -0,0 +1,30 @@
+
+from .modules import Module
+
+class Context:
+    def __init__(self, path):
+        self.path = path
+        self.subs = []
+
+    def add_sub(self, path):
+        sub = Subcontext(path, self)
+        self.subs.append(sub)
+
+        return sub
+
+    def resolve(self, path):
+        # This will raise an error if the module has not yet been initialized
+        return Module(self.path.joinpath(path).absolute())
+
+class Subcontext(Context):
+    def __init__(self, path, parent):
+        super().__init__(path)
+
+        self.parent = parent
+        self.module = Module(path.absolute(), self)
+
+    def resolve(self, path):
+        if path.startswith('/'):
+            return self.parent.resolve(path)
+        else:
+            return super().resolve(path)
--- sqfpack/modules.py
+++ sqfpack/modules.py
@@ -1,42 +1,129 @@
 
-class Factory:
+from pathlib import Path
+from collections import deque
+
+class Module:
     _instances = {}
 
-    def __new__(cls, path, *args, **kwargs):
-        if path in cls._instances:
+    def __new__(cls, path, initialize=False, *args, **kwargs):
+        try:
             inst = cls._instances[path]
+        except KeyError:
+            inst = super().__new__(cls)
+            inst.__init__(path, initialize, *args, **kwargs)
+            cls._instances[path] = inst
 
-            if isinstance(inst, cls):
-                return cls._instances[path]
+            return inst
+        else:
+            if initialize:
+                inst.initialize(*args, **kwargs)
+            
+            return inst
 
-        inst = super().__new__(cls)
-        inst.__init__(path, *args, **kwargs)
-        cls._instances[path] = inst
-
-        return inst
-
-
-class Section(Factory):
-    def __init__(self, path):
+    def __init__(self, path: Path, initialize, *args, **kwargs):
         self.path = path
-        self.modules = []
+        self.initalized = False
 
-    def add_module(self, module):
+        if initialize: self.initialize(*args, **kwargs):
+    
+    def initialize(
+        self,
+        ctx,
+        parent=None,
+        tag=None,
+        include=None,
+        preInit=None,
+        postInit=None,
+        config=None,
+        macros=None
+    ):
+
+        assert not self.initalized, (
+            '{} already initialized'.format(str(self)))
+
+        self.ctx = ctx
+        self.parent = parent
+        self.name = self.path.name
+
+        self.partial_tag = tag
+
+        if macros is not None:
+            self.macros = macros
+        else:
+            self.macros = {}
+
+        self.include = self._process_include(include)
+        self.preInit = preInit
+        self.postInit = postInit
+        self.config = config
+        self.initalized = True
+        self.functions = {}
+
+    def __str__(self):
+        return '{}({})'.format(type(self).__name__, str(self.path))
+
+    def add_function(self, path):
+        name = path.stem
+
+        self.functions[name] = {
+            'preInit': name in self.preInit,
+            'postInit': name in self.postInit
+        }
+
+        return self.functions[name]
+
+    def add_module(self, path, *args, **kwargs):
+        module = Module(path, True, self.ctx, self, *args, **kwargs)
         self.modules.append(module)
 
+        return module
 
-class Module(Factory):
-    def __init__(self, path):
-        self.path = path
+    def add_macro(self, name, repl, argc):
+        macro = {
+            'text': repl,
+            'args': ['ARG_' + str(idx + 1) for idx in range(argc)]
+        }
+        self.macros[name] = macro
 
+        return macro
 
-if __name__ == '__main__':
-    a = Section('a')
-    b = Section('b')
-    c = Section('a')
+    @property
+    def prefix_tag(self):
+        parent_tag = getattr(self.parent, 'prefix_tag', None)
+        if parent_tag is not None:
+            parent_tag += '_'
+        else:
+            parent_tag = ''
 
-    d = Module('a')
-    e = Module('b')
-    f = Module('a')
+        return parent_tag + self.partial_tag
 
-    print(c is a, b is a, d is a, f is d, f is c)
+    @property
+    def m_name_pretty(self):
+        m = self
+        name = m.name
+        name_dq = deque()
+
+        while name:
+            name_dq.appendleft(name)
+            m = getattr(m, 'parent', None)
+            name = getattr(m, 'name', None)
+
+        return '_'.join(name_dq)
+
+    def fn_name_real(self, func):
+        return '{prefix}_fnc_{func}'.format(
+            prefix=self.parent.prefix_tag, func=func)
+
+    def _process_include(self, include):
+        processed = []
+
+        if include is not None:
+            for i in include:
+                resolved = ctx.resolve(i)
+                processed.append(resolved)
+
+                self.add_macro(
+                    resolved.m_name_pretty,
+                    resolved.fn_name_real('##ARG_1'), 1)
+
+        return processed
