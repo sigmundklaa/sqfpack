14f961c19ec66546d59212b939a0df534d983700
SigJig <sigmundklaa@outlook.com>
2021-04-21 19:48:18 +0200

    added aliases to services

--- sandbox.py
+++ sandbox.py
@@ -1,7 +1,7 @@
 
 import os
 from sqfpack.context import Context
-
+from sqfpack.modules import Module
 
 def main(base):
     path = base.joinpath('src')
@@ -26,6 +26,18 @@ def main(base):
             srvc.add_sub(j, is_addon=True, is_module=True)
         else:
             print('File skipped {}'.format(i))
+
+    base_module = Module(srvc_path.joinpath('base'), False)
+    
+    if 'services' not in base_module.config:
+        base_module.config['services'] = {}
+
+    base_module.config['services']['aliases'] = {}
+
+    for f in os.listdir(srvc_path):
+        m = Module(srvc_path.joinpath(f), False)
+
+        base_module.config['services']['aliases'][m.source_name] = m.prefix_tag
 
     def _print(m, indent=0):
         def _fmt():
